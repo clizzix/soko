9b7b6f3f4ad1650bc5ba26c0e3c9e879a5121603
clizzix <godsilla1993@gmail.com>
2026-04-06 11:35:44 +0200

    created and implemented logo to the app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
         <div className="navbar bg-neutral text-neutral-content shadow-sm">
             <Link to="/" className="btn btn-ghost text-xl mx-auto">
-                SoKo
+                <img src={logo} alt="SoKo Logo" className="h-16" />
+                <p className="text-3xl font-bold">SoKo</p>
             </Link>
         </div>
     );
