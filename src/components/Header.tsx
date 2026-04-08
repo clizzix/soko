342e62a1da1804ee0c02b8f7db95f77deb18e4f8
clizzix <godsilla1993@gmail.com>
2026-04-08 09:35:05 +0200

    implemented Searchbar to search by Title, description or category tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router';
 import logo from '../assets/logo.png';
+import Searchbar from './Searchbar';
 
 const Header = () => {
     return (
-        <div className="navbar bg-neutral text-neutral-content shadow-sm">
-            <Link to="/" className="btn btn-ghost text-xl mx-auto">
+        <div className="navbar bg-neutral text-neutral-content shadow-sm justify-between">
+            <Link to="/" className="btn btn-ghost text-xl">
                 <img src={logo} alt="SoKo Logo" className="h-16" />
                 <p className="text-3xl font-bold">SoKo</p>
             </Link>
+            <Searchbar />
         </div>
     );
 };
